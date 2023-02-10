f40b924d03cc2f529e9f8dc468b3823bc4de7708
XJJ <hninee@163.com>
2023-02-10 14:10:14 +0800

    初始环境

---
+++ src/main/java/com/xiao/eatontime/EatontimeApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.eatontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatontimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatontimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiao/eatontime/EatontimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiao.eatontime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatontimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
